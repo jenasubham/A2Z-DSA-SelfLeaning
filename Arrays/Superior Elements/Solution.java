9bd1dc6fd6afc79301cd7d3d36caa01509af812a
Subham Jena <subhamjena4519@gmail.com>
2024-01-10 10:31:17 +0530

    Added the Brute force solution of Superior Elements

--- Arrays/Superior Elements/Solution.java
+++ Arrays/Superior Elements/Solution.java
@@ -1,4 +1,32 @@
-// __________________________________________________________________________
+// ________________________________________________________________ Time- O(N^2), No extra-space but for returning O(N) 
+// import java.util.*;
+// public class Solution {
+//     public static List< Integer > superiorElements(int []a) {
+//         int n = a.length;
+//         List<Integer> ans = new ArrayList<>(); 
+        
+//         for(int i=0; i<n; i++){
+//             boolean flag = true;
+//             for(int j=i+1; j<n;j++){
+//                 if(a[i] < a[j]){
+//                     flag = false;
+//                     break;
+//                 }
+//             }
+//             if(flag==true && !ans.contains(a[i])){  
+//                 ans.add(a[i]);
+//             }
+//         }
+        
+//         Collections.sort(ans);
+
+//         return ans;
+//     }
+
+// }
+
+
+// __________________________________________________________________________ Time- O(N), No extra-space but for returning O(N) 
 import java.util.*;
 public class Solution {
     public static List< Integer > superiorElements(int []a) {
