d5b9e226832fc0e0be6e7364b9038007ac62b557
Subham Jena <subhamjena4519@gmail.com>
2024-01-11 09:59:49 +0530

    Added the Optimal solution of Superior Elements using HashSet

--- Arrays/Longest Successive Elements/Solution.java
+++ Arrays/Longest Successive Elements/Solution.java
@@ -1,26 +1,63 @@
 // ________________________________________________________________________ O(N.logN) (due to Sorting)
+// import java.util.*;
+// public class Solution {
+//     public static int longestSuccessiveElements(int []a) {
+//         //sort the given Array
+//         Arrays.sort(a);
+
+//         int curr_count = 0;
+//         int last_smaller = Integer.MIN_VALUE;
+//         int max_count = 1;
+
+//         for(int i =0; i<a.length; i++){
+
+//             if(a[i] == last_smaller+1){
+//                 curr_count++;
+//                 last_smaller = a[i];
+//             }
+//             else if(a[i] != last_smaller){
+//                 curr_count = 1;
+//                 last_smaller = a[i];
+//             }
+//             max_count = Math.max(curr_count,max_count);
+//         }
+//         return max_count;
+//     }
+// }
+
+
+// _________________________________________________________________________ O(3N)- time complexity, O(N) Space complexity
 import java.util.*;
 public class Solution {
     public static int longestSuccessiveElements(int []a) {
-        //sort the given Array
-        Arrays.sort(a);
+        int n = a.length;
+        if(n==0) return 0;
 
-        int curr_count = 0;
-        int last_smaller = Integer.MIN_VALUE;
-        int max_count = 1;
+        Set<Integer> set = new HashSet<>();
 
-        for(int i =0; i<a.length; i++){
-
-            if(a[i] == last_smaller+1){
-                curr_count++;
-                last_smaller = a[i];
-            }
-            else if(a[i] != last_smaller){
-                curr_count = 1;
-                last_smaller = a[i];
-            }
-            max_count = Math.max(curr_count,max_count);
+        //put all the elements into the set;
+        for(int i=0;i<n; i++){
+            set.add(a[i]);
         }
-        return max_count;
+
+        // Intialize the longest as 1.
+        int  longest = 1;
+
+        for(int i :set){
+
+            int x = i;
+            int count = 1;
+            // if this is the first element of the sequence then only
+            if(! set.contains(i-1)){
+                // loop till the last element.
+                while(set.contains(x+1)){
+                    x++;
+                    count++;
+                }
+                longest = Math.max(longest,count);
+            }
+        }
+
+        return longest;
     }
 }
