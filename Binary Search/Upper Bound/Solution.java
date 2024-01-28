66a62fcfcbee581e9d1b4ace0e26113fafc4458b
Subham Jena <subhamjena4519@gmail.com>
2024-01-28 22:46:43 +0530

    Added the solution of  Upper Bound

---
+++ Binary Search/Upper Bound/Solution.java
@@ -0,0 +1,21 @@
+// ______________________________________________________ Time complexity- O(logn), Space complexity- O(1)
+public class Solution {
+    public static int upperBound(int []arr, int x, int n){
+        int start = 0;
+        int end = n-1;
+        int ans = n;
+
+        while(start <=end){
+            int mid = (start+end)/2;
+            if(arr[mid]>x){
+                ans=mid;
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return ans;
+
+    }
+}
