63824570bb476a120ee387c70f60257d4f9d6253
Subham Jena <subhamjena4519@gmail.com>
2024-01-30 00:53:52 +0530

    Added the better solution of  First and Last Position of an element.

--- Binary Search/First and Last Position of an Element/Solution.java
+++ Binary Search/First and Last Position of an Element/Solution.java
@@ -1,20 +1,79 @@
 // ______________________________________________________ time complexity- O(n), space complexity- O(1)
+// import java.util.*;
+// public class Solution {
+
+//     public static int[] firstAndLastPosition(ArrayList<Integer> arr, int n, int k) {
+//         int start = -1;
+//         int end = -1;
+
+//         for(int i=0;i<n;i++){
+//             if(arr.get(i)==k){
+//                 if(start == -1){
+//                 start = i;
+//                 }
+//                 end=i;  
+//             }
+//         }
+//         int[] sol = {start,end};
+//         return sol;
+//     }
+
+// };
+
+
+// _________________________________________________________ time complexity- O(logn), space complexity- O(1)
 import java.util.*;
 public class Solution {
 
-    public static int[] firstAndLastPosition(ArrayList<Integer> arr, int n, int k) {
-        int start = -1;
-        int end = -1;
+    static int lowerbound(ArrayList<Integer> arr,int n, int k){
+        int lower = n;
+        int start = 0;
+        int end = n-1;
 
-        for(int i=0;i<n;i++){
-            if(arr.get(i)==k){
-                if(start == -1){
-                start = i;
-                }
-                end=i;  
+        while(start<=end){
+            int mid = (start+end)/2;
+
+            if(arr.get(mid)>=k){
+                lower = mid;
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
             }
         }
-        int[] sol = {start,end};
+        return lower;
+    }
+
+    static int upperbound(ArrayList<Integer> arr, int n, int k){
+        int upper = n;
+        int start = 0;
+        int end = n-1;
+
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(arr.get(mid) > k){
+                upper= mid;
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return upper;
+    }
+
+    public static int[] firstAndLastPosition(ArrayList<Integer> arr, int n, int k) {
+        int lower = lowerbound(arr,n,k);
+        int upper = upperbound(arr,n,k);
+
+        if(lower == n || arr.get(lower) != k){
+            lower = -1;
+            upper = -1;
+            int sol[] = {-1,-1};
+            return sol;
+        }
+
+        int sol[] = {lower,upper-1};
         return sol;
     }
 
