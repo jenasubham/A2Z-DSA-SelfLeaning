c3ce1a0ea9683f64cad32aac8e8659ead416e908
Subham Jena <subhamjena4519@gmail.com>
2024-04-06 00:44:40 +0530

    Added the Optimal SOlution of Koko Eating Bananas.

--- Binary Search/Koko Eating Bananas/Solution.java
+++ Binary Search/Koko Eating Bananas/Solution.java
@@ -1,7 +1,37 @@
+// _______________________________________________________________ Time complexity- O( max element * n), space complexity- O(1) 
+// public class Solution {  
+//     public static int findMax(int v[]){
+//         int maxi = Integer.MIN_VALUE;
+
+//         for(int i=0; i<v.length; i++){
+//             if(v[i]> maxi) maxi = v[i];
+//         }
+//         return maxi;
+//     }
+//     public static int minimumRateToEatBananas(int []v, int h) {
+//         int maxi = findMax(v);
+//         for(int i=1; i<=maxi; i++){
+//             int sum = 0;
+
+//             for(int j=0;j<v.length; j++){
+//                 int temp = (int)Math.ceil((double)v[j] / i);
+//                 sum +=temp;
+//             }
+//             if(sum<= h){
+//                 return i;
+//             }
+//         }
+//         return maxi;
+//     }
+// }
+
+
+
+// _______________________________________________________________ Time complexity- O(n* log(max element)), space complexity- O(1)
+
 public class Solution {
     public static int findMax(int v[]){
-        int maxi = Integer.MIN_VALUE;
-
+        int maxi  = Integer.MIN_VALUE;
         for(int i=0; i<v.length; i++){
             if(v[i]> maxi) maxi = v[i];
         }
@@ -9,18 +39,26 @@ public class Solution {
     }
     public static int minimumRateToEatBananas(int []v, int h) {
         int maxi = findMax(v);
-        for(int i=1; i<=maxi; i++){
+        int perHr = maxi;
+        int start = 1;
+        int end = maxi;
+
+        while(start<= end){
+            int mid = start + (end-start)/2;
             int sum = 0;
 
-            for(int j=0;j<v.length; j++){
-                int temp = (int)Math.ceil((double)v[j] / i);
-                sum +=temp;
+            for(int i=0; i<v.length; i++){
+                sum += Math.ceil((double)v[i]/(double)mid);
             }
-            if(sum<= h){
-                return i;
+            if(sum <= h){
+                perHr = Math.min(mid,perHr);
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
             }
         }
-        return maxi;
+        return perHr;
     }
 }
 
