19dbbc391451aa8680c6b1c9e4a936a63d8558b8
Subham Jena <subhamjena4519@gmail.com>
2024-06-09 21:25:12 +0530

    Added the Optimal solution of Kth Missing Positive Number.

--- Binary Search/Kth Missing Positive Number/Solution.java
+++ Binary Search/Kth Missing Positive Number/Solution.java
@@ -1,14 +1,37 @@
 // _____________________________________________________________ Time complexity- O(n), space complexity- O(1)
+// public class Solution {
+//     public static int missingK(int[] arr, int n, int k) {
+//         for(int i=0; i<n; i++){
+//             if(arr[i]<= k){
+//                 k++;
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+//         return k;
+//     }
+// }
+
+
+// _______________________________________________________________ Time complexity- O(logn), space complexity- O(1)
 public class Solution {
     public static int missingK(int[] arr, int n, int k) {
-        for(int i=0; i<n; i++){
-            if(arr[i]<= k){
-                k++;
+        int start = 0;
+        int end = n-1;
+
+        while(start<=end){
+            int mid = start+(end-start);
+            int missing = arr[mid] - (mid+1);
+            if(missing < k){
+                start = mid+1;
             }
             else{
-                break;
+                end = mid-1;
             }
         }
-        return k;
+
+        //answer is arr[end]+ more we need. After solving the equation answer comeout to be high+1+k or low+k
+        return start+k;
     }
 }
