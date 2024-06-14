6419a35745c0890d110eda9f8d5fb6cace7ce094
Subham Jena <subhamjena4519@gmail.com>
2024-06-15 00:29:48 +0530

    Added the Optimal solution of Largest Subarray Sum Minimized.

--- Binary Search/Largest Subarray Sum Minimized/Solution.java
+++ Binary Search/Largest Subarray Sum Minimized/Solution.java
@@ -1,36 +1,80 @@
-// _____________________________________________________________ time complexity- O(N * (sum-max)), space complexity- O(1)
-public class Solution { 
-    static int countSubarray(int[] arr, int maxSum){
-        int partitions = 1;
-        int subarraySum = 0;
+// // _____________________________________________________________ time complexity- O(N * (sum-max)), space complexity- O(1)
+// public class Solution { 
+//     static int countSubarray(int[] arr, int maxSum){
+//         int partitions = 1;
+//         int subarraySum = 0;
 
+//         for(int i=0; i<arr.length; i++){
+//             if(subarraySum + arr[i]<= maxSum){
+//                 subarraySum +=arr[i];
+//             }
+//             else{
+//                 partitions++;
+//                 subarraySum = arr[i];
+//             }
+//         }
+//         return partitions;
+//     }
+
+//     public static int largestSubarraySumMinimized(int []arr, int k) {
+//         int low = Integer.MIN_VALUE;
+//         int high = 0;
+//         //find maximum and summation:
+//         for(int ele: arr){
+//             low = Math.max(low, ele);
+//             high+= ele;
+//         }
+
+//         for(int maxSum= low; maxSum<=high; maxSum++){
+//             if(countSubarray(arr,maxSum)== k){
+//                 return maxSum;
+//             }
+//         }
+//         return low;
+//     }
+// }
+
+// _____________________________________________________________ time complexity- O(N * log(sum-max)), space complexity- O(1)
+
+public class Solution {
+    public static int countPartitions(int[] arr, int maxSum){
+        int partitions = 1;
+        int subarraySum =0;
         for(int i=0; i<arr.length; i++){
-            if(subarraySum + arr[i]<= maxSum){
-                subarraySum +=arr[i];
+            if(subarraySum+ arr[i] <= maxSum){
+                //insert element to current subarray
+                subarraySum += arr[i];
             }
             else{
+                //insert element to next subarray
                 partitions++;
                 subarraySum = arr[i];
             }
         }
         return partitions;
     }
-
     public static int largestSubarraySumMinimized(int []arr, int k) {
-        int low = Integer.MIN_VALUE;
-        int high = 0;
-        //find maximum and summation:
+        int start = Integer.MIN_VALUE;
+        int end = 0;
+        //find the maximum and summation of the array. 
         for(int ele: arr){
-            low = Math.max(low, ele);
-            high+= ele;
+            start = Math.max(start, ele);
+            end += ele;
         }
+        
+        //Apply binary search:
+        while(start<= end){
+            int mid = start +(end-start)/2;
 
-        for(int maxSum= low; maxSum<=high; maxSum++){
-            if(countSubarray(arr,maxSum)== k){
-                return maxSum;
+            int partitions = countPartitions(arr, mid);
+            if(partitions> k){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
             }
         }
-        return low;
+        return start;
     }
 }
 
