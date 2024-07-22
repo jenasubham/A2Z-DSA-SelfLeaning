b26ab09e075063e5c5630b07d65477efd1065400
Subham Jena <subhamjena4519@gmail.com>
2024-07-22 23:50:01 +0530

    Added the BruteForce solution of  Search In A 2D Matrix Problem.

---
+++ Binary Search/Search In A 2D Matrix/Solution.java
@@ -0,0 +1,16 @@
+// ___________________________________________________________ time complexity- O(n*m), space complexity- O(1)
+import java.util.ArrayList;
+public class Solution {
+    static boolean searchMatrix(ArrayList<ArrayList<Integer>> mat, int target) {
+        int n = mat.size();
+        int m = mat.get(0).size();
+        for(int row = 0; row< n; row++){
+            for(int col = 0; col<m; col++){
+                if(mat.get(row).get(col) == target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
