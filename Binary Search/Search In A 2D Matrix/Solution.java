1536a0aefb72807fe995879b6d60783fc1195a98
Subham Jena <subhamjena4519@gmail.com>
2024-08-27 16:59:13 +0530

    Added the 2nd Better solution of  Search In A 2D Matrix Problem.

--- Binary Search/Search In A 2D Matrix/Solution.java
+++ Binary Search/Search In A 2D Matrix/Solution.java
@@ -16,20 +16,62 @@
 // }
 
 
+
+
 // ___________________________________________________________ time complexity- O(n + m), space complexity- O(1)
+// import java.util.ArrayList;
+// public class Solution {
+//     static boolean searchMatrix(ArrayList<ArrayList<Integer>> mat, int target) {
+//         int n = mat.size();
+//         int m = mat.get(0).size();
+
+//         for(int row=0; row<n; row++){
+//             if(mat.get(row).get(0) <= target && target <= mat.get(row).get(m-1)){
+//                 for(int col=0; col<m; col++){
+//                     if(mat.get(row).get(col)== target){
+//                         return true;
+//                     }
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+
+
+
+// _________________________________________________________ time complexity- O(n) + log(m), space complexity- O(1)
+
 import java.util.ArrayList;
 public class Solution {
+    
+    public static boolean binarySearch(ArrayList<Integer> arr, int target){
+        int start = 0;
+        int end = arr.size() -1;
+
+        while(start<= end){
+            int mid = start + (end-start)/2;
+
+            if(arr.get(mid) == target){
+                return true;
+            }
+            else if(target < arr.get(mid)){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return false;
+    }
+    
     static boolean searchMatrix(ArrayList<ArrayList<Integer>> mat, int target) {
         int n = mat.size();
         int m = mat.get(0).size();
-
         for(int row=0; row<n; row++){
             if(mat.get(row).get(0) <= target && target <= mat.get(row).get(m-1)){
-                for(int col=0; col<m; col++){
-                    if(mat.get(row).get(col)== target){
-                        return true;
-                    }
-                }
+                    return binarySearch(mat.get(row), target);
             }
         }
         return false;
