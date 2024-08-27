03ede23ae5af49e8ff440fba26e3682e79cb7d81
Subham Jena <subhamjena4519@gmail.com>
2024-08-27 19:20:34 +0530

    Added the Optimal solution of  Search In a 2D Matrix Problem.

--- Binary Search/Search In A 2D Matrix/Solution.java
+++ Binary Search/Search In A 2D Matrix/Solution.java
@@ -43,35 +43,69 @@
 
 // _________________________________________________________ time complexity- O(n) + log(m), space complexity- O(1)
 
+// import java.util.ArrayList;
+// public class Solution {
+    
+//     public static boolean binarySearch(ArrayList<Integer> arr, int target){
+//         int start = 0;
+//         int end = arr.size() -1;
+
+//         while(start<= end){
+//             int mid = start + (end-start)/2;
+
+//             if(arr.get(mid) == target){
+//                 return true;
+//             }
+//             else if(target < arr.get(mid)){
+//                 end = mid-1;
+//             }
+//             else{
+//                 start = mid+1;
+//             }
+//         }
+//         return false;
+//     }
+    
+//     static boolean searchMatrix(ArrayList<ArrayList<Integer>> mat, int target) {
+//         int n = mat.size();
+//         int m = mat.get(0).size();
+//         for(int row=0; row<n; row++){
+//             if(mat.get(row).get(0) <= target && target <= mat.get(row).get(m-1)){
+//                     return binarySearch(mat.get(row), target);
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+
+
+// _________________________________________________________________________ time complexity- log(n*m), space complexity- O(1)
+
 import java.util.ArrayList;
 public class Solution {
-    
-    public static boolean binarySearch(ArrayList<Integer> arr, int target){
-        int start = 0;
-        int end = arr.size() -1;
+    static boolean searchMatrix(ArrayList<ArrayList<Integer>> mat, int target) {
+        int n = mat.size();
+        int m = mat.get(0).size();
+        int length = n*m;
 
-        while(start<= end){
-            int mid = start + (end-start)/2;
+        int start = 0; 
+        int end = length-1;
 
-            if(arr.get(mid) == target){
-                return true;
-            }
-            else if(target < arr.get(mid)){
+        //apply binary search:
+        while(start <= end){
+            int mid = start + (end- start)/2;
+
+            int row = mid/m;
+            int col = mid%m;
+
+            if(target == mat.get(row).get(col)) return true;
+
+            else if( target < mat.get(row).get(col)){
                 end = mid-1;
             }
             else{
                 start = mid+1;
-            }
-        }
-        return false;
-    }
-    
-    static boolean searchMatrix(ArrayList<ArrayList<Integer>> mat, int target) {
-        int n = mat.size();
-        int m = mat.get(0).size();
-        for(int row=0; row<n; row++){
-            if(mat.get(row).get(0) <= target && target <= mat.get(row).get(m-1)){
-                    return binarySearch(mat.get(row), target);
             }
         }
         return false;
